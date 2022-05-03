787a6015e70f0d34c9305c664cc479f7e1a5e245
unknown <benjhassell@gmail.com>
2022-05-04 00:29:44 +0100

    added parser + changed functionality to use full file path + possibly deprecated previous tallying functionality

--- compare_directory_content.py
+++ compare_directory_content.py
@@ -1,32 +1,36 @@
 import os
+import argparse
 
 """
 This is the foundation for a file comparison software, future versions will likely scrap existing code and add far more.
 Currently does:
-- compare exact filename matches and tally after traversing every file in a directory.
+- compare exact filename matches and tally after traversing every file in a directory. (might not tally anymore with directory change)
 - duplicate identification
 
 Todo:
-1. compare from certain path downwards rather than just filename (i.e. may exist in other sub-directories)
-2. GUI:
+1. GUI:
     a. for selecting directories
     b. allowing you to open duplicates where mismatches occur (dependent on 1.)
-3. Duplicate file comparison based on contents (e.g. one file might be more up to date)
-4. Edit date comparison.
-5. Unit testing
+2. Duplicate file comparison based on contents (e.g. one file might be more up to date)
+3. Edit date comparison.
+4. Unit testing
 """
 
 def get_file_dict(directory):
     # Getting the current work directory (cwd)
     thisdir = directory
-
     g = {}
     # r=root, d=directories, f = files
-    for r, d, f in os.walk(thisdir):
-        for file in f:
-            #print(os.path.join(r, file))
+    for root, dirs, files in os.walk(thisdir):
+        for file in files:
+            #print(root, dirs, files)
+            driveless_directory = thisdir[3:] #Strip drive letter for file comparison (e.g. E:\)
+            file_path = os.path.join(root, file)
+            first_slash = file_path.find('\\')+1
+            full_path = file_path[first_slash:]
+            severed_path = full_path.replace(driveless_directory, "")
             #Counter for exact filename match. This should be changed at a later point to look at the matching directory header (e.g. /music/)
-            g[file] = g.get(file, 0) + 1
+            g[severed_path] = g.get(severed_path, 0) + 1
     ''' #Commented out snippet used to find the most excessively large amount of dupes, could become relevant later on.
     prev_highest = ("", 0)
     for key in g.keys():
@@ -52,8 +56,14 @@ def anotb(dict1, dict2, directory_1_label, directory_2_label):
             continue
     return not_in_d2, in_d2_but_differing_amounts
 
-dir1=r'test_directory_a'
-dir2=r'test_directory_b'
+parser = argparse.ArgumentParser(description='Directory paths')
+parser.add_argument("--dir1", type=str, default=r'test_directory_a')
+parser.add_argument("--dir2", type=str, default=r'test_directory_b')
+args = parser.parse_args()
+dir1 = args.dir1
+dir2 = args.dir2
+#print(dir1)
+#print(dir2)
 
 directory_a = get_file_dict(dir1)
 directory_b = get_file_dict(dir2)
